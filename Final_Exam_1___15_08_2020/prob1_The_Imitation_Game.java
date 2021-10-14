59e7b78babe43fb49f8f3ed345939bded3392772
Danail Valentinov Ivanov <89908414+Danail-Valentinov-Ivanov@users.noreply.github.com>
2021-10-15 00:30:11 +0300

    Add files via upload

---
+++ Final_Exam_1___15_08_2020/prob1_The_Imitation_Game.java
@@ -0,0 +1,39 @@
+package Final_Exam_1___15_08_2020;
+
+import java.util.Scanner;
+
+public class prob1_The_Imitation_Game {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder input = new StringBuilder(scanner.nextLine());
+        String command = scanner.nextLine();
+        while (!command.equals("Decode")) {
+            String[] currentCommand = command.split("\\|");
+            String commandLine = currentCommand[0];
+            switch (commandLine) {
+                case "Move":
+                    int numberOfLetters = Integer.parseInt(currentCommand[1]);
+                    while (numberOfLetters > 0) {
+                        input.append(input.charAt(0));
+                        input.deleteCharAt(0);
+                        numberOfLetters--;
+                    }
+                    break;
+                case "Insert":
+                    int index = Integer.parseInt(currentCommand[1]);
+                    String value = currentCommand[2];
+                    input.insert(index, value);
+                    break;
+                case "ChangeAll":
+                    String substring = currentCommand[1];
+                    String replacement = currentCommand[2];
+                    String temp = input.toString().replace(substring, replacement);
+                    input.setLength(0);
+                    input.append(temp);
+                    break;
+            }
+            command = scanner.nextLine();
+        }
+        System.out.printf("The decrypted message is: %s", input);
+    }
+}
