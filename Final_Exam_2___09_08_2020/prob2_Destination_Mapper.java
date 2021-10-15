f831f4564352b009622df2f6829e839382321490
Danail Valentinov Ivanov <89908414+Danail-Valentinov-Ivanov@users.noreply.github.com>
2021-10-16 00:46:20 +0300

    Add files via upload

---
+++ Final_Exam_2___09_08_2020/prob2_Destination_Mapper.java
@@ -0,0 +1,28 @@
+package Final_Exam_2___09_08_2020;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class prob2_Destination_Mapper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String regex = "(?<separator>=|\\/)(?<destination>[A-Z][A-Za-z]{2,})\\1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        List<String> listDestinations = new ArrayList<>();
+
+        while (matcher.find()) {
+            listDestinations.add(matcher.group("destination"));
+        }
+        int points = 0;
+        for (var e : listDestinations) {
+            points += e.length();
+        }
+        System.out.printf("Destinations: %s\n", String.join(", ", listDestinations));
+        System.out.printf("Travel Points: %d", points);
+    }
+}
