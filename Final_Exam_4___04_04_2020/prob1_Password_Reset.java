b4817952c81aef04128f9cb14c9b6a709215f20a
Danail Valentinov Ivanov <89908414+Danail-Valentinov-Ivanov@users.noreply.github.com>
2021-10-18 00:11:47 +0300

    Add files via upload

---
+++ Final_Exam_4___04_04_2020/prob1_Password_Reset.java
@@ -0,0 +1,48 @@
+package Final_Exam_4___04_04_2020;
+
+import java.util.Scanner;
+
+public class prob1_Password_Reset {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder rawPassword = new StringBuilder(scanner.nextLine());
+        String commandLine = scanner.nextLine();
+        while (!commandLine.equals("Done")) {
+            String[] inputCommand = commandLine.split(" ");
+            switch (inputCommand[0]) {
+                case "TakeOdd":
+                    String newString = "";
+                    for (int i = 1; i < rawPassword.length(); i += 2) {
+                        char character = rawPassword.charAt(i);
+                        newString += character;
+                    }
+                    rawPassword.replace(0, rawPassword.length(), newString);
+                    System.out.println(rawPassword);
+                    break;
+                case "Cut":
+                    int startIndex = Integer.parseInt(inputCommand[1]);
+                    int length = Integer.parseInt(inputCommand[2]);
+                    int endIndex = startIndex + length;
+                    String substring = rawPassword.substring(startIndex, endIndex);
+                    int index = rawPassword.indexOf(substring);
+                    rawPassword.replace(index, index + substring.length(), "");
+                    System.out.println(rawPassword);
+                    break;
+                case "Substitute":
+                    String substringForReplace = inputCommand[1];
+                    String substitute = inputCommand[2];
+                    if (rawPassword.toString().contains(substringForReplace)) {
+                        String helpString = rawPassword.toString()
+                                .replace(substringForReplace, substitute);
+                        rawPassword.replace(0, rawPassword.length(), helpString);
+                        System.out.println(rawPassword);
+                    } else {
+                        System.out.println("Nothing to replace!");
+                    }
+                    break;
+            }
+            commandLine = scanner.nextLine();
+        }
+        System.out.printf("Your password is: %s", rawPassword);
+    }
+}
