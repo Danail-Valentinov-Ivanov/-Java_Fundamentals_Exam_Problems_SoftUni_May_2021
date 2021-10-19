06822c536e1c026cb099fddfe4b21dceead26ad6
Danail Valentinov Ivanov <89908414+Danail-Valentinov-Ivanov@users.noreply.github.com>
2021-10-20 00:48:39 +0300

    Add files via upload

---
+++ Final_Exam_Archive_10_January_2019_Part_II/prob3_Santas_Secret_Helper.java
@@ -0,0 +1,30 @@
+package Final_Exam_Archive_10_January_2019_Part_II;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class prob3_Santas_Secret_Helper {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int key = Integer.parseInt(scanner.nextLine());
+        String input = scanner.nextLine();
+        String regex = "@(?<children>[A-Za-z]+)[^@\\-!:>]*!(?<behavior>[G]|[N])!";
+        Pattern pattern = Pattern.compile(regex);
+        while (!input.equals("end")) {
+            String helpString = "";
+            for (int i = 0; i < input.length(); i++) {
+                int currentChar = input.charAt(i) - key;
+                char currentSymbol = (char) currentChar;
+                helpString += currentSymbol;
+            }
+            Matcher matcher = pattern.matcher(helpString);
+            if (matcher.find()){
+                if (matcher.group("behavior").equals("G")){
+                    System.out.println(matcher.group("children"));
+                }
+            }
+            input = scanner.nextLine();
+        }
+    }
+}
